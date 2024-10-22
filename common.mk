b9f4701bf548e90c5cf3360fd38d2cd80f2df08b
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-10-22 18:27:44 +0900

    sm6150-common: Add back legacy hidl android.hardware.bluetooth.audio for compat
    Change-Id: I871df7f469ce8ac88ff8ce20769ad2bd539f1c3d

--- common.mk
+++ common.mk
@@ -46,6 +46,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio@2.2-impl \
     android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
